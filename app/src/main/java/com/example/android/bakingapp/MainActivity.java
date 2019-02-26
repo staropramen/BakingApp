b4d27c504d3839e3b943f146ce12a9f5d467a4c1
Daniel Spiess <dspiess82@gmail.com>
2019-02-26 17:07:11 -0300

    Add Data Binding to App

--- app/src/main/java/com/example/android/bakingapp/MainActivity.java
+++ app/src/main/java/com/example/android/bakingapp/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.android.bakingapp;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.android.bakingapp.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Setup Data Binding
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        mBinding.tvText.setText("LETS GOOOOO");
     }
 }
