be68a3d8ebaa83883c04bfe0eee0b24700fe8fb9
Daniel Spiess <dspiess82@gmail.com>
2019-02-26 21:42:15 -0300

    Add GetDataService interface

--- app/src/main/java/com/example/android/bakingapp/network/GetDataService.java
+++ app/src/main/java/com/example/android/bakingapp/network/GetDataService.java
@@ -1,4 +1,12 @@
 package com.example.android.bakingapp.network;
 
+import com.example.android.bakingapp.model.Recipe;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
 public interface GetDataService {
+    @GET
+    Call<Recipe> getAllRecipes();
 }
